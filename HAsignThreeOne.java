b369891e45a9e2bd347a3f6e5fa0177654e9f099
ITSOK\anonm <dev1ender@gmail.com>
2017-06-24 02:57:33 +0530

    Added TaskOne

---
+++ HAsignThreeOne.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+public class HAsignThreeOne {
+
+	public static void main(String[] args)
+		throws IOException, URISyntaxException  {
+	
+		// TODO Auto-generated method stub
+		String uri = "/";
+		 //1. Get the Configuration instance
+	    Configuration configuration = new Configuration();
+	    //2. Get the instance of the HDFS
+	    FileSystem hdfs = FileSystem.get(URI.create(uri), configuration);
+	    //3. Get the metadata of the desired directory
+	    FileStatus[] fileStatus = hdfs.listStatus(new Path(uri));
+	    //5. Iterate through the directory and display the files in it
+	    for(FileStatus file : fileStatus)
+	    {
+	      System.out.println(file.getPath().getName());
+	    }
+	}
+  
+	}
+
