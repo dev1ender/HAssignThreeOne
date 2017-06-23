c7b399ee2f3d4861ce2d0e54078b7428990dd845
ITSOK\anonm <dev1ender@gmail.com>
2017-06-24 03:49:48 +0530

    final commit

--- HAsignThreeOneOne.java
+++ HAsignThreeOneOne.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-public class HAsignThreeOne {
+public class HAsignThreeOneOne{
 
 	public static void main(String[] args)
 		throws IOException, URISyntaxException  {
